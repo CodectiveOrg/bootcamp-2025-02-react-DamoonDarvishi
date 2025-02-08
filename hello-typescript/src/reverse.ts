fba09deb8c847603abcc1e4395c8f572088d1997
damoon darvishi <damon.darvishi@gmail.com>
2025-02-08 11:32:35 +0100

    Updated: reverse array

--- hello-typescript/src/reverse.ts
+++ hello-typescript/src/reverse.ts
@@ -1,35 +1,10 @@
 // Reverse any array
-export function reverseArray<T>(array: T[]): T[] {
-  const reverseAnything: T[] = [];
-  for (let i = 0; i < array.length; i++) {
-    reverseAnything.push(array[array.length - 1 - i]);
+export function reverseArray<T>(items: T[]): T[] {
+  const result: T[] = [];
+
+  for (let i = items.length - 1; i >= 0; i--) {
+    result.push(items[i]);
   }
-  console.log(reverseAnything);
-  return reverseAnything;
+
+  return result;
 }
-
-reverseArray([1, 2, "three", "four", "5", 6]);
-
-// Reverse a number array
-export function reverseNumbersArray(array: number[]): number[] {
-  const reverseNumbers: number[] = [];
-  for (let i = 0; i < array.length; i++) {
-    reverseNumbers.push(array[array.length - 1 - i]);
-  }
-  console.log(reverseNumbers);
-  return reverseNumbers;
-}
-
-reverseNumbersArray([2, 4, 6, 8]); // Targert: [8, 6, 4, 2]
-
-// Reverse a string array
-export function reverseStringsArray(array: string[]): string[] {
-  const reverseString: string[] = [];
-  for (let i = 0; i < array.length; i++) {
-    reverseString.push(array[array.length - 1 - i]);
-  }
-  console.log(reverseString);
-  return reverseString;
-}
-
-reverseStringsArray(["A", "B", "C", "D"]); // ["D", "C", "B", "A"]
