735d50c301448cdfd6ba47673ffd8c8ec1b46eae
damoon darvishi <damon.darvishi@gmail.com>
2025-02-08 11:33:13 +0100

    Add: mock data for sort

---
+++ hello-typescript/src/sort/entities.data.ts
@@ -0,0 +1,32 @@
+import type { IEntity } from "./entity.type";
+
+export const entities: IEntity[] = [
+  {
+    title: "First Post",
+    body: "This is the first post",
+    createdAt: "2020-01-01",
+    modifiedAt: "2025-01-01",
+  },
+  {
+    title: "Second Post",
+    body: "This is the second post",
+    createdAt: "2022-10-01",
+  },
+  {
+    title: "Third Post",
+    body: "This is the third post",
+    createdAt: "2023-11-01",
+    modifiedAt: "2024-12-01",
+  },
+  {
+    title: "Fourth Post",
+    body: "This is the fourth post",
+    createdAt: "2017-10-01",
+  },
+  {
+    title: "Fifth Post",
+    body: "This is the fifth post",
+    createdAt: "2020-01-01",
+    modifiedAt: "2024-11-01",
+  },
+];
