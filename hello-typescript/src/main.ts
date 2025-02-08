b6a2987adb9ea366be7bb05970f9bd8ad4ea0bbe
damoon darvishi <damon.darvishi@gmail.com>
2025-02-08 11:34:40 +0100

    Updated: call task-2 functions in main

--- hello-typescript/src/main.ts
+++ hello-typescript/src/main.ts
@@ -1,5 +1,28 @@
-async function main(): Promise<void> {
-  console.log("Hello, friend!");
+import { countNumbersArray } from "./countNumbers";
+import { reverseArray } from "./reverse";
+import { entities } from "./sort/entities.data";
+import { sortEntities } from "./sort/sortEntities";
+
+function testReverseArray(): void {
+  console.log(reverseArray([1, 2, "three", "four", "5", 6]));
 }
 
-main().then();
+function testCountNumbers(): void {
+  const items = [3, 5, "Vue", 3, "React", "Angular"];
+  const count = countNumbersArray(items);
+  console.log(count);
+}
+
+function testSortEntities(): void {
+  const sortedEntities = sortEntities(entities);
+
+  console.log(sortedEntities);
+}
+
+function main(): void {
+  testReverseArray();
+  testCountNumbers();
+  testSortEntities();
+}
+
+main();
